3f83e9abce39985421856f1750ad830d29a04413
Sk Kaif Rahaman <skkaifrahaman112@gmail.com>
2024-10-19 11:28:18 +0530

    remove Google API from github

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -8,14 +8,15 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCtQAmRtfVrcqlQPqi-AQ4sMPeoyvAzLdo',
-  authDomain: 'todo-app-58fb9.firebaseapp.com',
-  projectId: 'todo-app-58fb9',
-  storageBucket: 'todo-app-58fb9.appspot.com',
-  messagingSenderId: '897475900624',
-  appId: '1:897475900624:web:dbf3d9324bc6cc14dc5ad5',
-  measurementId: 'G-1X5M16DE4M',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
